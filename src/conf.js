7adbb51cf3c86511f15f862b5df0fcbd4b16b761
Rohan Dhar <53666199+rohan-dhar@users.noreply.github.com>
2020-05-05 07:04:48 +0530

    Delete conf.js

--- src/conf.js
+++
@@ -1,5 +0,0 @@
-export const GEOCODE_URL = "https://maps.googleapis.com/maps/api/geocode/json";
-export const GEOCODE_KEY = "AIzaSyBil-e17ZjyMVP-M9_BB0scF6QGTbIzjrA";
-
-export const DISTRICT_URL = "https://api.covid19india.org/v2/state_district_wise.json";
-export const STATE_URL = "https://api.rootnet.in/covid19-in/stats/history";
